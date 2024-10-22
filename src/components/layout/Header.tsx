68c2d68ad8d3f0d1298ca645fe007c6d46a22e81
Angel <angel.pashov@proton.me>
2024-10-22 18:33:42 +0300

    navigation added in the header section

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,62 @@
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+
+
+
+const Header = (): JSX.Element => {
+    return (
+        <AppBar position="fixed" color="inherit">
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography sx={{fontFamily: 'Facon', fontSize: '1.8rem'}}>SportPulse</Typography>
+                <Box sx={{ display: 'flex', gap: '20px' }}>
+                    <Button color="inherit"
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '10px 20px',
+                            transition: '0.3s',
+                            fontFamily: 'Montserrat',
+                            fontWeight: 'bold',
+                            fontSize: '1.2rem',
+                            '&:hover': {
+                                backgroundColor: '#686666',
+                                color: '#fff',
+                            },
+                        }}>
+                        Home
+                    </Button>
+                    <Button color="inherit"
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '10px 20px',
+                            transition: '0.3s',
+                            fontFamily: 'Montserrat',
+                            fontWeight: 'bold',
+                            fontSize: '1.2rem',
+                            '&:hover': {
+                                backgroundColor: '#686666',
+                                color: '#fff',
+                            }
+                        }}>
+                        Events
+                    </Button>
+                    <Button color="inherit"
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '10px 20px',
+                            transition: '0.3s',
+                            fontFamily: 'Montserrat',
+                            fontWeight: 'bold',
+                            fontSize: '1.2rem',
+                            '&:hover': {
+                                backgroundColor: '#686666',
+                                color: '#fff',
+                            }
+                        }}>
+                        Live Scores
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
