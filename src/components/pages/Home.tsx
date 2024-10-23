a2b72ff92091dc3868da932f3c32151e46972d22
Angel <angel.pashov@proton.me>
2024-10-23 16:01:46 +0300

    Added overlay to the home box

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,33 +1,84 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 const Home = (): JSX.Element => {
     return (
-        <Box sx={{ margin: '0', padding: '0', width: '100%', position: 'relative' }}>
-            {/* Sports Banner */}
-            <Box
-                component="img"
-                src="/AdobeStock_286933595_Preview.jpeg" // Path to your sports banner image
-                alt="Sports Banner"
-                sx={{ width: '100%', height: 'auto', objectFit: 'cover', }}
-            />
+        <Box
+            sx={{
+                margin: '0',
+                padding: '0',
+                width: '100%',
+                height: '70vh',
+                position: 'relative',
+                backgroundImage: `url('/AdobeStock_286933595_Preview.jpeg')`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignContent: 'space-around',
+            }}>
+            <Box sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0.4)',
+                zIndex: 1
+            }}/>
+
             <Typography
                 variant="h4"
                 component="h1"
                 sx={{
-                    position: 'absolute',
-                    top: '25%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)', // Center the text
+                    position: 'relative',
                     color: 'white',
-                    textShadow: '1px 1px 4px rgba(0, 0, 0, 0.7)',  // Shadow for better visibility
-                    zIndex: 1, // Ensure text is above the image
+                    textShadow: '1px 1px 4px rgba(0, 0, 0, 0.7)',
+                    zIndex: 2,
                     fontFamily: 'Radley Sans',
-                    fontSize: '3rem',
+                    fontSize: '3.5rem',
+                    textAlign: 'center',
                 }}
             >
                 The best sport <br /> event website!
             </Typography>
-
+            <Box sx={{
+                width: '40%',
+                borderBottom: '4px solid white',
+                borderRadius: '20px',
+                margin: '20px auto 0',
+                zIndex: 2
+            }} />
+            <Typography component="p" sx={{
+                textAlign: 'center',
+                marginTop: '20px',
+                fontSize: '1.3rem',
+                color: 'white',
+                fontFamily: 'Montserrat',
+                zIndex: 2
+            }}>
+                Experience the thrill of live sports events <br /> and stay updated  with real-time scores and updates. <br /> Join us and be part of the excitement!
+            </Typography>
+            <Button color='inherit' sx={{
+                backgroundColor: '#00A4CC',
+                '&:hover': {
+                    backgroundColor: '#007BAC',
+                },
+                top: '7%',
+                padding: '12px 30px', 
+                borderRadius: '20px', 
+                color: 'white', 
+                textTransform: 'none', 
+                boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.2)', 
+                transition: 'background-color 0.3s',
+                zIndex: 2,
+                fontFamily: 'Montserrat',
+                fontWeight: 'bold',
+                fontSize: '1.1rem',
+            }}>
+                JOIN NOW!
+            </Button>
         </Box>
     );
 };
