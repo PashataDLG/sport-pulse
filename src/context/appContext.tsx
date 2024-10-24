83157516fc0315d97fcbcb512e184d55d4b3d036
Angel <angel.pashov@proton.me>
2024-10-25 00:46:32 +0300

    Added context

---
+++ src/context/appContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from 'react';
+
+interface AppContextValue {
+    isDrawerOpen: boolean;
+    setIsDrawerOpen: Dispatch<SetStateAction<boolean>>;
+    handleDrawerToggle: (event: Event, reason?: string) => void;
+}
+
+const AppContext = createContext<AppContextValue | undefined>(undefined);
+
+export const useAppContext = () => {
+    const appContext = useContext(AppContext);
+    if (!appContext) {
+        throw new Error('useAppContext must be used within AppContextProvider');
+    };
+
+    return appContext;
+};
+
+interface AppContextProviderProps {
+    children: ReactNode;
+};
+
+type Event = React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>;
+
+
+export const АppContextProvider = ({ children }: AppContextProviderProps) => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleDrawerToggle = (event: Event, reason?: string) => {
+        if (isDrawerOpen && reason === 'backdropClick') {
+            console.log(reason);
+            setIsDrawerOpen(false);
+        } else if (!isDrawerOpen) {
+            setIsDrawerOpen(true);
+        }
+    };
+
+    const values = {
+        isDrawerOpen,
+        setIsDrawerOpen,
+        handleDrawerToggle
+    };
+
+    return (
+        <AppContext.Provider value={values}>
+        { children }
+        </AppContext.Provider>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import theme from './theme/theme.ts'
 import { ThemeProvider } from '@mui/material/styles'
+import { АppContextProvider } from './context/appContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+	<StrictMode>
+		<АppContextProvider>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</АppContextProvider>
+	</StrictMode>,
 );
