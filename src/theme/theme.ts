cd9ebbe4bc6eadee87fae35b9883105e6c6988a1
Angel <angel.pashov@proton.me>
2024-10-24 19:05:36 +0300

    fixed brake points

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -6,7 +6,7 @@ const theme = createTheme({
             xs: 0,    
             sm: 600,     
             md: 768,      
-            lg: 1024,    
+            lg: 1024,
             xl: 1440   
         }
     },
