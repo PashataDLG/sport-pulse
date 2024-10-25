f6249d83203244d665a057612a68d454e159cf6b
Angel <angel.pashov@proton.me>
2024-10-25 19:37:34 +0300

    updated upcoming event widget

--- src/components/common/UpcomingEvents.tsx
+++ src/components/common/UpcomingEvents.tsx
@@ -1,32 +1,84 @@
 import React from 'react';
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemText, Typography, Avatar } from '@mui/material';
+import dateFormatter from '../../utils/dateFormatter';
 
 interface Event {
-  id: number;
-  name: string;
-  date: string;
-  time: string;
+	id: number;
+	name: string;
+	date: string;
+	time: string;
 }
 
 interface UpcomingEventsProps {
-  events: Event[];
+	events: Event[];
 }
 
 const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events }) => {
-  return (
-    <Box sx={{ maxWidth: 360, bgcolor: 'background.paper', borderRadius: '16px', overflow: 'hidden', boxShadow: 3 }}>
-      <Typography sx={{ p: 2, bgcolor: 'primary.main', color: 'primary.contrastText' }} variant="h6" component="div">
-        Upcoming Events
-      </Typography>
-      <List component="nav" aria-label="upcoming sports events">
-        {events.map(event => (
-          <ListItem key={event.id} component="button">
-            <ListItemText primary={event.name} secondary={`${event.date} at ${event.time}`} />
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+	return (
+		<Box sx={{ width: 350, bgcolor: 'background.paper', borderRadius: '16px', overflow: 'hidden', boxShadow: 3, display: 'flex', flexDirection: 'column', gap: '1px' }}>
+			<Typography sx={{ p: 2, bgcolor: 'primary.main', color: 'primary.contrastText', fontFamily: 'Montserrat' }} variant="h6" component="div">
+				Upcoming Events
+			</Typography>
+			<List aria-label="upcoming sports events" sx={{
+				overflow: 'auto',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '5px',
+			}}>
+				{events.map(event => (
+					<ListItem key={event.id} component="button" sx={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						gap: '5px'
+					}}>
+
+						<Typography>Bolton vs Peterboro</Typography>
+
+						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvxxrv1448808301.png' />
+						<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+							<Typography variant="body2" color="text.secondary" sx={{
+								fontFamily: 'Montserrat',
+							}}>
+								{event.time}
+							</Typography>
+							<Typography variant="body2" color="text.secondary" sx={{
+								fontFamily: 'Montserrat',
+							}}>
+								{dateFormatter(event.date)}
+							</Typography>
+						</Box>
+						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/tyxsvt1424033035.png' />
+					</ListItem>
+				))}
+			</List>
+			<Button sx={{
+				borderRadius: '20px',
+				fontFamily: 'Montserrat',
+				fontWeight: 'bolder',
+				fontSize: '1rem',
+				backgroundColor: '#00A4CC',
+				color: '#fff',
+				'&:hover': {
+					backgroundColor: '#007BAC',
+				},
+			}}>
+				View All Events
+			</Button>
+			<Button sx={{
+				borderRadius: '20px',
+				fontFamily: 'Montserrat',
+				fontWeight: 'bolder',
+				fontSize: '1rem',
+				backgroundColor: '#00A4CC',
+				color: '#fff',
+				'&:hover': {
+					backgroundColor: '#007BAC',
+				},
+			}}>
+				Buy Tickets
+			</Button>
+		</Box>
+	);
 };
 
 export default UpcomingEvents;
