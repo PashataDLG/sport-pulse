47ca815da218e1913715e909d89de4893b325df2
Angel <angel.pashov@proton.me>
2024-10-25 19:37:45 +0300

    rendering the upcoming event widget

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,27 +5,26 @@ import UpcomingEvents from '../common/UpcomingEvents';
 
 const events = [
     { id: 1, name: 'Football Match', date: '2024-10-30', time: '19:00' },
-    { id: 2, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
-  ];
+    { id: 3, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
+    { id: 4, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
+    { id: 5, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
+    { id: 6, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
+    { id: 7, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
+];
 
 const Home = (): JSX.Element => {
     return (
         <Container maxWidth={false} sx={{ width: '100vw', padding: '0 !important' }}>
             <Banner />
-            <UpcomingEvents events={events} />
-            <Box sx={{ py: 4 }}>
-                {Array.from(new Array(10)).map((_, index) => (
-                    <Box key={index} sx={{ margin: 'auto', maxWidth: 600, p: 2, borderBottom: '1px solid #ccc' }}>
-                        <Typography variant="h6" gutterBottom>
-                            Heading {index + 1}
-                        </Typography>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et euismod ligula. Morbi mattis pretium interdum. Morbi vel dui nec magna elementum pellentesque non ac purus. Cras et erat in libero ultricies congue. Vivamus fermentum felis nec velit pharetra accumsan. Cras sit amet metus sit amet velit lacinia ullamcorper. Nam facilisis, massa vel pharetra aliquam, tellus velit fringilla ligula, a venenatis ipsum orci eget dui.
-                        </Typography>
-                    </Box>
-                ))}
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                padding: '10px',  
+                }}>
+                    < UpcomingEvents events={ events } />
             </Box>
-        </Container>
+
+        </Container >
     );
 };
 
