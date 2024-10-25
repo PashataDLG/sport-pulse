1846048edba413714e2fd404283330eb880ebc39
Angel <angel.pashov@proton.me>
2024-10-25 12:32:20 +0300

    Created a new banner component

---
+++ src/components/common/Banner.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Typography } from "@mui/material";
+import { Parallax } from "react-parallax";
+
+const Banner: React.FC = (): JSX.Element => {
+    return (
+        <Parallax
+            bgImage='https://t3.ftcdn.net/jpg/06/07/07/80/360_F_607078002_yMGIjR7oCK8fvvR8qD8hZ5EsXK7V8M7I.jpg'
+            strength={500}
+            bgImageStyle={{ height: '100%', width: '100%' }}
+        >
+            <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <Box
+                    sx={{
+                        width: '100%',
+                        height: '70vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-around',
+                        alignItems: 'center',
+                        position: 'relative',
+                    }}>
+                    <Box sx={{
+                        position: 'absolute',
+                        width: '100%',
+                        height: '150%',
+                        backgroundColor: 'rgba(0, 0, 0, 0.4)',
+                        zIndex: 1
+                    }} />
+                    <Typography
+                        variant="h4"
+                        component="h1"
+                        sx={{
+                            color: 'white',
+                            textShadow: '1px 1px 4px rgba(0, 0, 0, 0.7)',
+                            zIndex: 2,
+                            fontFamily: 'Radley Sans',
+                            fontSize: '3.5rem',
+                            textAlign: 'center',
+                            mt: 2,
+                            mb: 1
+                        }}
+                    >
+                        The best sport <br /> event website!
+                    </Typography>
+                    <Typography component="p" sx={{
+                        textAlign: 'center',
+                        fontSize: '1.3rem',
+                        color: 'white',
+                        fontFamily: 'Montserrat',
+                        zIndex: 2,
+                        mb: 2
+                    }}>
+                        Experience the thrill of live sports events <br /> and stay updated with real-time scores and updates. <br /> Join us and be part of the excitement!
+                    </Typography>
+                    <Button color='inherit' sx={{
+                        backgroundColor: '#00A4CC',
+                        '&:hover': {
+                            backgroundColor: '#007BAC',
+                        },
+                        padding: '12px 30px',
+                        borderRadius: '20px',
+                        color: 'white',
+                        textTransform: 'none',
+                        boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.2)',
+                        transition: 'background-color 0.3s',
+                        zIndex: 2,
+                        fontFamily: 'Montserrat',
+                        fontWeight: 'bold',
+                        fontSize: '1.1rem',
+                        mt: 1
+                    }}>
+                        JOIN NOW!
+                    </Button>
+                </Box>
+            </div>
+        </Parallax>
+    );
+};
+
+export default Banner;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,80 +1,11 @@
 import { Parallax } from 'react-parallax';
 import { Box, Button, Container, Typography } from '@mui/material';
+import Banner from '../common/Banner';
 
 const Home = (): JSX.Element => {
     return (
         <Container maxWidth={false} sx={{ width: '100vw', padding: '0 !important' }}>
-            <Parallax
-                bgImage='https://t3.ftcdn.net/jpg/06/07/07/80/360_F_607078002_yMGIjR7oCK8fvvR8qD8hZ5EsXK7V8M7I.jpg'
-                strength={500}
-                bgImageStyle={{ height: '100%', width: '100%' }}
-            >
-                <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                    <Box
-                        sx={{
-                            width: '100%',
-                            height: '70vh',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            position: 'relative',
-                        }}>
-                        <Box sx={{
-                            position: 'absolute',
-                            width: '100%',
-                            height: '150%',
-                            backgroundColor: 'rgba(0, 0, 0, 0.4)',
-                            zIndex: 1
-                        }} />
-                        <Typography
-                            variant="h4"
-                            component="h1"
-                            sx={{
-                                color: 'white',
-                                textShadow: '1px 1px 4px rgba(0, 0, 0, 0.7)',
-                                zIndex: 2,
-                                fontFamily: 'Radley Sans',
-                                fontSize: '3.5rem',
-                                textAlign: 'center',
-                                mt: 2, 
-                                mb: 1
-                            }}
-                        >
-                            The best sport <br /> event website!
-                        </Typography>
-                        <Typography component="p" sx={{
-                            textAlign: 'center',
-                            fontSize: '1.3rem',
-                            color: 'white',
-                            fontFamily: 'Montserrat',
-                            zIndex: 2,
-                            mb: 2
-                        }}>
-                            Experience the thrill of live sports events <br /> and stay updated with real-time scores and updates. <br /> Join us and be part of the excitement!
-                        </Typography>
-                        <Button color='inherit' sx={{
-                            backgroundColor: '#00A4CC',
-                            '&:hover': {
-                                backgroundColor: '#007BAC',
-                            },
-                            padding: '12px 30px',
-                            borderRadius: '20px',
-                            color: 'white',
-                            textTransform: 'none',
-                            boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.2)',
-                            transition: 'background-color 0.3s',
-                            zIndex: 2,
-                            fontFamily: 'Montserrat',
-                            fontWeight: 'bold',
-                            fontSize: '1.1rem',
-                            mt: 1
-                        }}>
-                            JOIN NOW!
-                        </Button>
-                    </Box>
-                </div>
-            </Parallax>
+            <Banner />
             <Box sx={{ py: 4 }}>
                 {Array.from(new Array(10)).map((_, index) => (
                     <Box key={index} sx={{ margin: 'auto', maxWidth: 600, p: 2, borderBottom: '1px solid #ccc' }}>
