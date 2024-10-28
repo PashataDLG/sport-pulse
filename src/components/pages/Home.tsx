2360beda5e2ed6e22d15aa0b9bbcda25b8cf9d41
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-10-28 18:07:58 +0000

    added live events and removed unused imports

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,25 @@
+<<<<<<< HEAD
 import { Box, Container, useMediaQuery } from '@mui/material';
 import Banner from '../common/Banner';
 import UpcomingEvents from '../common/UpcomingEvents';
 import LiveEvents from '../common/LiveEvents';
 import theme from '../../theme/theme';
 import Standings from '../common/Standings';
+=======
+import { Box, Container } from '@mui/material';
+import Banner from '../common/Banner';
+import UpcomingEvents from '../common/UpcomingEvents';
+import LiveScoreWiget from '../common/LiveScoreWidget';
+
+interface LiveEvent {
+    id: number;
+    homeTeam: string;
+    awayTeam: string;
+    scoreHomeTeam: number;
+    scoreAwayTeam: number;
+    time: string;
+};
+>>>>>>> 4be922f (added live events and removed unused imports)
 
 const events = [
     { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
@@ -26,8 +42,30 @@ const liveEvents = [
     { id: 6, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' },
 ];
 
+const liveEvents: LiveEvent[] = [
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00' },
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00' },
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00' },
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00' },
+];
+
+
 const Home = (): JSX.Element => {
+<<<<<<< HEAD
     const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+=======
+    return (
+        <Container maxWidth={false} sx={{ width: '100vw', padding: '0 !important' }}>
+            <Banner />
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                padding: '10px',
+            }}>
+                < UpcomingEvents events={events} />
+                <LiveScoreWiget events={liveEvents} />
+            </Box>
+>>>>>>> 4be922f (added live events and removed unused imports)
 
     return (
         <Container maxWidth={false} sx={{ width: '100%', padding: '0 !important' }}>
