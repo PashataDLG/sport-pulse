b5e831be61bd3776fb9c92bccad493cacdbfcd09
Angel <angel.pashov@proton.me>
2024-10-31 15:06:35 +0200

    added live event widget with hard coded data

---
+++ src/components/common/LiveEvents.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Box, Button, List, ListItem, Typography, Avatar } from '@mui/material';
+
+interface LiveEvent {
+	id: number;
+    homeTeam: string;
+    awayTeam: string;
+    homeTeamGoals: number;  
+    awayTeamGoals: number;
+    time: string;
+}
+
+interface LiveEventsProps {
+	events: LiveEvent[];
+}
+
+const LiveEvents: React.FC<LiveEventsProps> = ({ events }) => {
+	return (
+		<Box sx={{ width: 350, bgcolor: 'background.paper', borderRadius: '16px', overflow: 'hidden', boxShadow: 3, display: 'flex', flexDirection: 'column', gap: '1px', maxHeight: '80vh' }}>
+			<Typography sx={{ p: 2, bgcolor: 'primary.main', color: 'primary.contrastText', fontFamily: 'Montserrat' }} variant="h6" component="div">
+				Live Events
+			</Typography>
+			<List aria-label="upcoming sports events" sx={{
+				overflow: 'auto',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '5px',
+			}}>
+				{events.map(event => (
+					<ListItem key={event.id} component="button" sx={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						gap: '5px'
+					}}>
+
+						<Typography>{event.homeTeam}</Typography>
+
+						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvxxrv1448808301.png' />
+						<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+							<Typography variant="body2" color="text.secondary" sx={{
+								fontFamily: 'Montserrat',
+							}}>
+								{event.time}
+							</Typography>
+							<Typography variant="body2" color="text.secondary" sx={{
+								fontFamily: 'Montserrat',
+                                fontSize: '1.1rem',
+							}}>
+                                {event.homeTeamGoals} - {event.awayTeamGoals}
+							</Typography>
+						</Box>
+						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/tyxsvt1424033035.png' />
+						<Typography>{event.awayTeam}</Typography>
+					</ListItem>
+				))}
+			</List>
+			<Button sx={{
+				borderRadius: '20px',
+				fontFamily: 'Montserrat',
+				fontWeight: 'bolder',
+				fontSize: '1rem',
+				backgroundColor: '#00A4CC',
+				color: '#fff',
+				'&:hover': {
+					backgroundColor: '#007BAC',
+				},
+			}}>
+				View All Live Events
+			</Button>
+		</Box>
+	);
+};
+
+export default LiveEvents;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Parallax } from 'react-parallax';
 import { Box, Button, Container, Typography } from '@mui/material';
 import Banner from '../common/Banner';
 import UpcomingEvents from '../common/UpcomingEvents';
+import LiveEvents from '../common/LiveEvents';
 
 const events = [
     { id: 1, name: 'Football Match', date: '2024-10-30', time: '19:00' },
@@ -12,16 +13,28 @@ const events = [
     { id: 7, name: 'Basketball Game', date: '2024-11-05', time: '20:30' },
 ];
 
+const liveEvents = [
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 2, awayTeamGoals: 0, time: '43:54' }, 
+    { id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' }, 
+    { id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' }, 
+    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' }, 
+    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' }, 
+    { id: 6, homeTeam: 'Liverpool', awayTeam: 'Chelsea', homeTeamGoals: 1, awayTeamGoals: 0, time: '43:54' }, 
+];
+
 const Home = (): JSX.Element => {
     return (
         <Container maxWidth={false} sx={{ width: '100vw', padding: '0 !important' }}>
             <Banner />
             <Box sx={{
                 display: 'flex',
-                justifyContent: 'space-around',
-                padding: '10px',  
+                justifyContent: 'center',
+                justifyItems: 'center',
+                padding: '10px',
+                gap: '40px'
                 }}>
-                    < UpcomingEvents events={ events } />
+                    <UpcomingEvents events={ events } />
+                    <LiveEvents events={ liveEvents } />
             </Box>
 
         </Container >
