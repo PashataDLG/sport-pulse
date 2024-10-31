8c46303f35f1ec7890cfbe8e449ecbefbe171a9f
Angel <angel.pashov@proton.me>
2024-10-31 22:51:22 +0200

    edited standings

--- src/components/common/Standings.tsx
+++ src/components/common/Standings.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Box, Avatar } from '@mui/material';
 
 const Standings: React.FC = () => {
-    const leaderboardData = [
+    const standingsData = [
         { rank: 1, name: 'Liverpool', points: 23, logo: 'https://www.thesportsdb.com/images/media/team/badge/ar5tn91728915882.png', goalDiff: '+23', playedGames: 10 },
         { rank: 2, name: 'Arsenal', points: 22, logo: 'https://www.thesportsdb.com/images/media/team/badge/uyhbfe1612467038.png', goalDiff: '+23', playedGames: 10 },
         { rank: 3, name: 'Chelsea', points: 22, logo: 'https://www.thesportsdb.com/images/media/team/badge/yvwvtu1448813215.png', goalDiff: '+23', playedGames: 10 },
@@ -33,6 +33,7 @@ const Standings: React.FC = () => {
                 marginTop: '0px',
                 width: '100%',
                 alignSelf: 'center',
+                fontSize: '2rem',
             }}>
                 Standings
             </Typography>
@@ -48,7 +49,7 @@ const Standings: React.FC = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {leaderboardData.map((row) => (
+                        {standingsData.map((row) => (
                             <TableRow key={row.rank}>
                                 <TableCell>{row.rank}</TableCell>
                                 <TableCell>
