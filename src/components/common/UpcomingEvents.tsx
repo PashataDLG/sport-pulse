8485f122914f5932deca3c2b818049c877cebd03
Angel <angel.pashov@proton.me>
2024-11-01 11:15:44 +0200

    upcoming events component edited

--- src/components/common/UpcomingEvents.tsx
+++ src/components/common/UpcomingEvents.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Box, Button, List, ListItem, Typography, Avatar } from '@mui/material';
+import { Box, Button, List, ListItem, Typography, Avatar, Paper, Table, TableBody, TableCell, TableContainer, TableRow, useMediaQuery } from '@mui/material';
 import dateFormatter from '../../utils/dateFormatter';
+import theme from '../../theme/theme';
 
 interface Event {
 	id: number;
-	name: string;
-	date: string;
+	homeTeam: string;
+	awayTeam: string;
 	time: string;
+	date: string;
 }
 
 interface UpcomingEventsProps {
@@ -14,85 +16,176 @@ interface UpcomingEventsProps {
 }
 
 const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events }) => {
-
+	const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
 
 	return (
-		<Box sx={{ maxWidth: '30rem', bgcolor: 'background.paper', borderRadius: '16px', overflow: 'hidden', boxShadow: 3, display: 'flex', flexDirection: 'column', gap: '1px', maxHeight: '80vh' }}>
-			<Typography sx={{ p: 2, bgcolor: 'primary.main', color: 'primary.contrastText', fontFamily: 'Montserrat', textAlign: 'center', fontWeight: 'bold' }} variant="h6" component="div">
+		<>
+		{isMobile ? (
+			<Box sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			bgcolor: 'background.paper',
+			width: '100%'
+		}}>
+			<Typography variant="h4" gutterBottom sx={{
+				fontFamily: 'Montserrat',
+				fontWeight: 'bold',
+				color: '#ffffff',
+				textAlign: 'center',
+				backgroundColor: '#1976d2',
+				borderRadius: '20px',
+				marginBottom: '20px',
+				marginTop: '0px',
+				width: '100%',
+				alignSelf: 'center',
+				fontSize: '2rem',
+			}}>
 				Upcoming Events
 			</Typography>
-			<List aria-label="upcoming sports events" sx={{
-				overflow: 'auto',
-				display: 'flex',
-				flexDirection: 'column',
-				gap: '5px',
+			<TableContainer component={Paper} sx={{
+				maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px'
 			}}>
-				{events.map(event => (
-					<ListItem key={event.id} component="button" sx={{
-						display: 'flex',
-						justifyContent: 'space-between',
-						gap: '5px'
-					}}>
+				<Table stickyHeader>
 
-						<Typography>Bolton</Typography>
-
-						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvxxrv1448808301.png' />
-						<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-							<Typography variant="body2" color="text.secondary" sx={{
-								fontFamily: 'Montserrat',
-								fontSize: '1.1rem',
-							}}>
-								{event.time}
-
-							</Typography>
-							<Typography variant="body2" color="text.secondary" sx={{
-								fontFamily: 'Montserrat',
-							}}>
-								{dateFormatter(event.date)}
-							</Typography>
-						</Box>
-						<Avatar src='https://www.thesportsdb.com/images/media/team/badge/tyxsvt1424033035.png' />
-						<Typography>Peterboro</Typography>
-					</ListItem>
-				))}
-			</List>
+					<TableBody>
+						{events.map((event) => (
+							<TableRow key={event.id}>
+								<TableCell>
+									<Box sx={{
+										display: 'flex',
+										flexDirection: 'column',
+										justifyContent: 'center',
+										alignItems: 'center',
+										gap: '5px'
+									}}>
+										<Avatar src='https://www.thesportsdb.com/images/media/team/badge/ar5tn91728915882.png' />{event.homeTeam}
+									</Box>
+								</TableCell>
+								<TableCell>
+									<Box sx={{
+										display: 'flex',
+										flexDirection: 'column',
+										alignItems: 'center',
+										gap: '5px',
+										padding: '20px',
+										marginTop: '-30px'
+									}}>
+										<Typography sx={{ fontSize: '0.9rem', color: 'text.secondary' }}>
+											{dateFormatter(event.date)}
+										</Typography>
+										<Box sx={{
+											display: 'flex',
+											justifyContent: 'center',
+											alignItems: 'center',
+											gap: '10px',
+										}}>
+											<Typography sx={{ fontSize: '1.1rem' }}>
+												{event.time}
+											</Typography>
+										</Box>
+									</Box>
+								</TableCell>
+								<TableCell>
+									<Box sx={{
+										display: 'flex',
+										flexDirection: 'column',
+										justifyContent: 'center',
+										alignItems: 'center',
+										gap: '5px',
+									}}>
+										<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvwvtu1448813215.png' />{event.awayTeam}
+									</Box>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				</TableContainer>
+			</Box>
+		) : (
 			<Box sx={{
 				display: 'flex',
-				flexDirection: 'row',
-				justifyContent: 'space-evenly',
-				gap: '20px',
-				paddingBottom: '10px',
-				paddingRight: '10px',
-				paddingLeft: '10px',
+				flexDirection: 'column',
+				bgcolor: 'background.paper',
+				width: '400px'
 			}}>
-				<Button sx={{
-					borderRadius: '20px',
+				<Typography variant="h4" gutterBottom sx={{
 					fontFamily: 'Montserrat',
-					fontWeight: 'bolder',
-					fontSize: '0.9rem',
-					backgroundColor: '#00A4CC',
-					color: '#fff',
-					'&:hover': {
-						backgroundColor: '#007BAC',
-					},
-				}}>
-					View All Events
-				</Button>
-				<Button sx={{
+					fontWeight: 'bold',
+					color: '#ffffff',
+					textAlign: 'center',
+					backgroundColor: '#1976d2',
 					borderRadius: '20px',
-					fontFamily: 'Montserrat',
-					fontWeight: 'bolder',
-					fontSize: '0.9rem',
-					backgroundColor: '#00A4CC',
-					color: '#fff',
-					'&:hover': {
-						backgroundColor: '#007BAC',
-					},
+					marginBottom: '20px',
+					marginTop: '0px',
+					width: '100%',
+					alignSelf: 'center',
+					fontSize: '2rem',
 				}}>
-					Buy Tickets
-				</Button>
-			</Box>
+					Upcoming Events
+				</Typography>
+				<TableContainer component={Paper} sx={{
+					maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px'
+				}}>
+					<Table stickyHeader>
+	
+						<TableBody>
+							{events.map((event) => (
+								<TableRow key={event.id}>
+									<TableCell>
+										<Box sx={{
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+											gap: '5px'
+										}}>
+											<Avatar src='https://www.thesportsdb.com/images/media/team/badge/ar5tn91728915882.png' />{event.homeTeam}
+										</Box>
+									</TableCell>
+									<TableCell>
+										<Box sx={{
+											display: 'flex',
+											flexDirection: 'column',
+											alignItems: 'center',
+											gap: '5px',
+											padding: '20px',
+											marginTop: '-30px'
+										}}>
+											<Typography sx={{ fontSize: '0.9rem', color: 'text.secondary' }}>
+												{dateFormatter(event.date)}
+											</Typography>
+											<Box sx={{
+												display: 'flex',
+												justifyContent: 'center',
+												alignItems: 'center',
+												gap: '10px',
+											}}>
+												<Typography sx={{ fontSize: '1.1rem' }}>
+													{event.time}
+												</Typography>
+											</Box>
+										</Box>
+									</TableCell>
+									<TableCell>
+										<Box sx={{
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+											gap: '5px',
+										}}>
+											<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvwvtu1448813215.png' />{event.awayTeam}
+										</Box>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
 		</Box>
+		)}
+		</>
 	);
 };
 
