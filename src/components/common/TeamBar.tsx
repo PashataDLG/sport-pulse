7e8b096ef37c4f702a141ec25c43dc11a8f2b49a
Angel <angel.pashov@proton.me>
2024-11-04 14:58:10 +0200

    mobile responsiveness added to team bar

--- src/components/common/TeamBar.tsx
+++ src/components/common/TeamBar.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Box, Grid, Toolbar } from "@mui/material";
+import { AppBar, Box, Grid, Toolbar, useMediaQuery } from "@mui/material";
 import Tooltip from '@mui/material/Tooltip';
+import theme from "../../theme/theme";
 
 interface Team {
     id: number;
@@ -11,41 +12,79 @@ interface teams {
 }
 
 const TeamAppBar: React.FC<teams> = ({ teams }): JSX.Element => {
+    const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+
     return (
-        <Box sx={{ marginTop: '50px', borderTop: '2px solid white' }}>
-            <AppBar position="static" color="default" sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'row',
-                backgroundColor: '#f5f5f5',
-                maxHeight: '100px',
-            }}>
-                <Toolbar>
-                    <Grid container spacing={1} justifyContent="r">
-                        {teams.map((team) => (
-                            <Tooltip title="Liverpool" placement="bottom-start">
-                                <Box
-                                    component="img"
-                                    src={team.crest}
-                                    alt="team crest"
-                                    sx={{
-                                        width: 38,
-                                        height: 38,
-                                        paddingLeft: '20px',
-                                        paddingTop: '20px',
-                                        transition: 'transform 0.3s ease-in-out',
-                                        '&:hover': {
-                                            transform: 'scale(1.3)',
-                                            cursor: 'pointer'
-                                        }
-                                    }}
-                                />
-                            </Tooltip>
-                        ))}
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <>
+            {isMobile ? (<Box sx={{ marginTop: '50px', borderTop: '2px solid white' }}>
+                <AppBar position="static" color="default" sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    backgroundColor: '#f5f5f5',
+                    maxHeight: '50px',
+                }}>
+                    <Toolbar sx={{ overflowX: 'auto', whiteSpace: 'nowrap', height: '100px' }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            {teams.map((team) => (
+                                <Tooltip key={team.id} title="Liverpool" placement="bottom-start">
+                                    <Box
+                                        component="img"
+                                        src={team.crest}
+                                        alt="team crest"
+                                        sx={{
+                                            width: 38,
+                                            height: 38,
+                                            marginLeft: '20px',
+                                            marginTop: '20px',
+                                            [theme.breakpoints.down('sm')]: {
+                                                width: 30,
+                                                height: 30,
+                                                marginLeft: '10px',
+                                                marginTop: '10px',
+                                            }
+                                        }}
+                                    />
+                                </Tooltip>
+                            ))}
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Box>) : (<Box sx={{ marginTop: '50px', borderTop: '2px solid white' }}>
+                <AppBar position="static" color="default" sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    backgroundColor: '#f5f5f5',
+                    maxHeight: '100px',
+                }}>
+                    <Toolbar>
+                        <Grid container spacing={1} justifyContent="r">
+                            {teams.map((team) => (
+                                <Tooltip title="Liverpool" placement="bottom-start">
+                                    <Box
+                                        component="img"
+                                        src={team.crest}
+                                        alt="team crest"
+                                        sx={{
+                                            width: 38,
+                                            height: 38,
+                                            paddingLeft: '20px',
+                                            paddingTop: '20px',
+                                            transition: 'transform 0.3s ease-in-out',
+                                            '&:hover': {
+                                                transform: 'scale(1.3)',
+                                                cursor: 'pointer'
+                                            }
+                                        }}
+                                    />
+                                </Tooltip>
+                            ))}
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </Box>)}
+        </>
     )
 }
 
