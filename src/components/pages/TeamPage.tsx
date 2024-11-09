bf3d972ad4db710a6404a6d591f2565fd2d1adfa
Angel <angel.pashov@proton.me>
2024-11-09 14:05:32 +0200

    finished the teampage banner

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -1,20 +1,66 @@
 import { Box, Container, Typography } from '@mui/material'
-import React from 'react'
+import { MdStadium } from "react-icons/md";
+
 
 const TeamPage = (): JSX.Element => {
 	return (
 		<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-			<Box sx={{ width: '90%', height: '50vh', bgcolor: 'inherit', display: 'flex', border:'1px solid #f3f3f3' }}>
-				<Box sx={{ width: '70%', height: '100%' }}>
-					<Box sx={{ display: 'flex', alignContent: 'flex-start' }}>
-						{/* Content goes here */}
+			<Box sx={{ width: '90%', height: '50vh', bgcolor: 'inherit', display: 'flex', border: '1px solid #f3f3f3' }}>
+				<Box
+					sx={{
+						width: '70%',
+						height: '100%',
+						padding: '100px 0px 0px 65px',
+						display: 'flex',
+						alignItems: 'flex-start',
+					}}
+				>
+					<Box
+						sx={{
+							display: 'grid',
+							gridTemplateColumns: 'auto 2fr',
+							alignItems: 'flex-start',
+							gap: '20px',
+							height: '100%',
+						}}
+					>
+						<img
+							src="https://sportal365images.com/process/smp-images-production/assets/team/logo/92-Liverpool-logo.png"
+							alt="Liverpool Logo"
+							style={{ width: '160px', height: 'auto' }}
+						/>
+						<Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px', paddingLeft: '50px', paddingTop: '15px' }}>
+							<Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '2rem', fontFamily: 'Montserrat', bottom: '10px' }}>
+								Liverpool
+							</Typography>
+							<Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px', color: 'white' }}>
+								<Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+									<img src="https://sportal365images.com/process/smp-images-production/assets/country/flag/15-England-flag.png" alt="England Flag" style={{ width: '20px', height: 'auto', }} />
+									<Typography sx={{ fontFamily: 'Montserrat', fontSize: '1rem', fontWeight: 'bold' }}>
+										England
+									</Typography>
+								</Box>
+								<Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+									<img src="https://sportal365images.com/process/smp-images-production/assets/15082022/87501e33-d82d-4098-854c-9e50259d7c6f.png" alt="Premier League Icon" style={{ width: '20px', height: 'auto' }} />
+									<Typography sx={{ fontFamily: 'Montserrat', fontSize: '1rem', fontWeight: 'bold' }}>
+										Premier League
+									</Typography>
+								</Box>
+								<Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+									<MdStadium size={20}/>
+									<Typography sx={{ fontFamily: 'Montserrat', fontSize: '1rem', fontWeight: 'bold' }}>
+										"Anfield" | Liverpool
+									</Typography>
+								</Box>
+							</Box>
+						</Box>
 					</Box>
 				</Box>
 				<Box sx={{ width: '50%', height: '100%', display: 'flex' }}>
-					<img src="/public/2024-25-squad-photo-30092024-full.jpg" style={{width: '100%', }}  alt="" />
+					<img src="/public/2024-25-squad-photo-30092024-full.jpg" style={{ width: '100%', }} alt="" />
 				</Box>
 			</Box>
-		</Container>
+		</Container >
 	)
 }
 
