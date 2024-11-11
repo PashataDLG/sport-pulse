53cc2458a873e2df7453e13aec7f9551b00c5867
Angel <angel.pashov@proton.me>
2024-11-11 14:57:48 +0200

    Added upcoming events to the team page.

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -1,11 +1,25 @@
 import { Container } from '@mui/material'
 import TeamBanner from '../common/TeamBanner';
+import UpcomingEvents from '../common/UpcomingEvents';
+
+const teamUpcomingEvents = [
+    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 6, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 7, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 8, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 9, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+];
 
 
 const TeamPage = (): JSX.Element => {
 	return (
-		<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+		<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '20px', flexDirection: 'column' }}>
 			<TeamBanner />
+			<UpcomingEvents events={teamUpcomingEvents} page='team' />
 		</Container >
 	)
 }
