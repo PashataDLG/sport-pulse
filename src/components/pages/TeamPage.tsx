f27ccf0531bf01d65b63ebcbd0c11a13e4f4b967
Angel <angel.pashov@proton.me>
2024-11-11 15:03:47 +0200

    added changes to team page, so the layout is better

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -6,18 +6,14 @@ const teamUpcomingEvents = [
     { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
     { id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
     { id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 6, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 7, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 8, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 9, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' }, 
+    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' }, 
 ];
 
 
 const TeamPage = (): JSX.Element => {
 	return (
-		<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '20px', flexDirection: 'column' }}>
+		<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '10%', flexDirection: 'column', gap: '10%' }}>
 			<TeamBanner />
 			<UpcomingEvents events={teamUpcomingEvents} page='team' />
 		</Container >
