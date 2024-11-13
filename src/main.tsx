1797320d8fc70549bb4f8c665879f57a2841538d
Angel <angel.pashov@proton.me>
2024-11-13 16:23:54 +0200

    context provided

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import theme from './theme/theme.ts'
 import { ThemeProvider } from '@mui/material/styles'
 import { АppContextProvider } from './context/appContext.tsx'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { TeamPageProvider } from './context/teamPageContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Router>
-			<АppContextProvider>
-				<ThemeProvider theme={theme}>
-					<App />
-				</ThemeProvider>
-			</АppContextProvider>
+			<TeamPageProvider>
+				<АppContextProvider>
+					<ThemeProvider theme={theme}>
+						<App />
+					</ThemeProvider>
+				</АppContextProvider>
+			</TeamPageProvider>
 		</Router>
 	</StrictMode>,
 );
