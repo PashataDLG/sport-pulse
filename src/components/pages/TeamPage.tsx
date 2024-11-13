20043aa73c3a5aa0bf33e4af8865158d78df81bf
Angel <angel.pashov@proton.me>
2024-11-13 16:24:21 +0200

    team page now reneders a component based on the button clicked

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -2,32 +2,42 @@ import { Container, useMediaQuery } from '@mui/material'
 import TeamBanner from '../common/TeamBanner';
 import UpcomingEvents from '../common/UpcomingEvents';
 import TeamPageBar from '../common/TeamPageBar';
+import { useTeamPageContext } from '../../context/teamPageContext';
 
 const teamUpcomingEvents = [
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' }, 
-    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' }, 
+	{ id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+	{ id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+	{ id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+	{ id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+	{ id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
 ];
 
 
 const TeamPage = (): JSX.Element => {
 	const isMobile = useMediaQuery('(max-width:600px)');
+	const { selectedButton } = useTeamPageContext();
+
+	const renderContent = () => {
+		switch (selectedButton) {
+			case 0: return <UpcomingEvents events={teamUpcomingEvents} page='team' />; break;
+			case 1: return <div>Live Events component</div>; break;
+			case 2: return <div>Standings component</div>; break;
+		}
+	}
 
 	return (
 		<>
-			{isMobile ? 
-				<Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', padding: '0'  }}>
+			{isMobile ?
+				<Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', padding: '0' }}>
 					<TeamBanner />
 					<TeamPageBar />
-					<UpcomingEvents events={teamUpcomingEvents} page='team' />
+					{renderContent()}
 				</Container >
 				:
 				<Container maxWidth="xl" sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', marginTop: '14%', flexDirection: 'column', gap: '10%', alignItems: 'center' }}>
 					<TeamBanner />
 					<TeamPageBar />
-					<UpcomingEvents events={teamUpcomingEvents} page='team' />
+					{renderContent()}
 				</Container >
 			}
 		</>
