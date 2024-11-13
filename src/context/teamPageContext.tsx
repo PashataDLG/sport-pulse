c85315951e50f4dfe33b7013e2be14f093d54825
Angel <angel.pashov@proton.me>
2024-11-13 16:23:42 +0200

    fixed the team page context to work as expected

--- src/context/teamPageContext.tsx
+++ src/context/teamPageContext.tsx
@@ -1,18 +1,31 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState, ReactNode, useContext } from 'react';
 
 interface TeamPageContextValue {
     selectedButton: number | null;
-    setSelectedButton: (index: number) => void;
+    handleButtonClick: (index: number) => void;
+};
+
+export const useTeamPageContext = () => {
+    const teamContext = useContext(TeamPageContext);
+    if (!teamContext) {
+        throw new Error('useTeamPageContext must be used within TeamPageProvider');
+    };
+
+    return teamContext;
 };
 
 export const TeamPageContext = createContext<TeamPageContextValue | undefined>(undefined);
 
-export const TeamPageProvider = ({ children }: { children: React.ReactNode }) => {
+export const TeamPageProvider = ({ children }: { children: ReactNode }) => {
     const [selectedButton, setSelectedButton] = useState<number | null>(null);
+
+    const handleButtonClick = (index: number) => {
+        setSelectedButton(index);
+    };
 
     const values = {
         selectedButton,
-        setSelectedButton,
+        handleButtonClick,
     };
 
     return (
