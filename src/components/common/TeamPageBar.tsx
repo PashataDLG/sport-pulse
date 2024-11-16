504019dfebe6f4ad92614b5b5eb4e269443a5f46
Angel <angel.pashov@proton.me>
2024-11-16 17:57:46 +0200

    Added modern style to the team page buttons

--- src/components/common/TeamPageBar.tsx
+++ src/components/common/TeamPageBar.tsx
@@ -9,10 +9,10 @@ const TeamPageBar = () => {
     const {selectedButton, handleButtonClick } = useTeamPageContext();
 
     const buttons = [
-        { label: 'Team News', icon: <FaRegNewspaper size={18} style={{ marginRight: '8px' }} /> },
-        { label: 'Upcoming Events', icon: <MdOutlineNextWeek size={18} style={{ marginRight: '8px' }} /> },
-        { label: 'Live Events', icon: <CgMediaLive size={18} style={{ marginRight: '8px' }} /> },
-        { label: 'Standings', icon: <IoMdPodium size={18} style={{ marginRight: '8px' }} /> },
+        { label: 'Team News', icon: <FaRegNewspaper size={18} style={{ marginRight: '8px', color: '#1976d2' }} /> },
+        { label: 'Upcoming Events', icon: <MdOutlineNextWeek size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
+        { label: 'Live Events', icon: <CgMediaLive size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
+        { label: 'Standings', icon: <IoMdPodium size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
     ]
  
     return (
@@ -23,11 +23,10 @@ const TeamPageBar = () => {
                     onClick={() => handleButtonClick(index)}
                     sx={{
                         padding: '10px 20px',
-                        backgroundColor: selectedButton === index ? '#044863' : '#1976d2',
+                        color: '#1976d2',
                         borderRadius: '20px',
-                        transition: 'background-color 0.4s',
                         '&:hover': {
-                            backgroundColor: '#044863',
+                            color: '#1976d2',
                         },
                     }}>
                     <Typography sx={{
