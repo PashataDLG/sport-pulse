f654af842bdeb5518c978a6be005f4844d3e116d
Angel <angel.pashov@proton.me>
2024-11-17 14:05:19 +0200

    Added more breakpoints, and made sure the team page bar is looking good on all screens

--- src/components/common/TeamPageBar.tsx
+++ src/components/common/TeamPageBar.tsx
@@ -1,22 +1,28 @@
-import { Box, Button, Typography } from '@mui/material';
-import { MdOutlineNextWeek } from 'react-icons/md';
-import {  useTeamPageContext } from '../../context/teamPageContext';
-import { IoMdPodium } from "react-icons/io";
-import { CgMediaLive } from "react-icons/cg";
-import { FaRegNewspaper } from "react-icons/fa6";
+import {Box, Button, Typography} from '@mui/material';
+import {MdOutlineNextWeek} from 'react-icons/md';
+import {useTeamPageContext} from '../../context/teamPageContext';
+import {IoMdPodium} from "react-icons/io";
+import {CgMediaLive} from "react-icons/cg";
+import {FaRegNewspaper} from "react-icons/fa6";
 
 const TeamPageBar = () => {
-    const {selectedButton, handleButtonClick } = useTeamPageContext();
+    const {handleButtonClick} = useTeamPageContext();
 
     const buttons = [
-        { label: 'Team News', icon: <FaRegNewspaper size={18} style={{ marginRight: '8px', color: '#1976d2' }} /> },
-        { label: 'Upcoming Events', icon: <MdOutlineNextWeek size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
-        { label: 'Live Events', icon: <CgMediaLive size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
-        { label: 'Standings', icon: <IoMdPodium size={18} style={{ marginRight: '8px', color: '#1976d2'  }} /> },
+        {label: 'Team News', icon: <FaRegNewspaper size={18} style={{marginRight: '8px', color: '#1976d2'}}/>},
+        {label: 'Upcoming Events', icon: <MdOutlineNextWeek size={18} style={{marginRight: '8px', color: '#1976d2'}}/>},
+        {label: 'Live Events', icon: <CgMediaLive size={18} style={{marginRight: '8px', color: '#1976d2'}}/>},
+        {label: 'Standings', icon: <IoMdPodium size={18} style={{marginRight: '8px', color: '#1976d2'}}/>},
     ]
- 
+
     return (
-        <Box sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'column', md: 'column', lg: 'row' }, gap: '15px', maxWidth: '100%', marginTop: '20px' }}>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: {xs: 'column', sm: 'column', md: 'column', lg: 'column', xl: 'row'},
+            gap: '15px',
+            maxWidth: '100%',
+            marginTop: { zFold5: '25%', xs: '50%', sm: '40%', md: '30%', lg: '20px', iphoneSE: '55%', iphone12ProMax: '25%', surfaceDuo: '50%'},
+        }}>
             {buttons.map((button, index) => (
                 <Button
                     key={index}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -7,21 +7,27 @@ declare module '@mui/material/styles' {
         md: true;
         lg: true;
         xl: true;
+        zFold5: true;
         iphoneSE: true;
+        iphone12ProMax: true;
         iphone14ProMax: true;
+        surfaceDuo: true;
     }
 }
 
 const theme: Theme = createTheme({
     breakpoints: {
         values: {
-            xs: 0,    
-            sm: 600,     
-            md: 768,      
+            xs: 0,
+            zFold5: 344,
+            iphoneSE: 375,
+            iphone12ProMax: 390,
+            iphone14ProMax: 430,
+            surfaceDuo: 540,
+            sm: 600,
+            md: 768,
             lg: 1024,
             xl: 1440,
-            iphoneSE: 375,
-            iphone14ProMax: 430,
         }
     },
 });
