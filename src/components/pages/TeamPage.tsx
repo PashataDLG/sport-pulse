d4f08d27fc813d99b51bbf5d2ff988d3f4b5fba9
Angel <angel.pashov@proton.me>
2024-11-17 13:30:23 +0200

    added is mobile break point to the team page

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -1,33 +1,69 @@
-import { Box, Container, useMediaQuery } from '@mui/material'
+import {Box, Container, useMediaQuery} from '@mui/material'
 import TeamBanner from '../common/TeamBanner';
 import UpcomingEvents from '../common/UpcomingEvents';
 import TeamPageBar from '../common/TeamPageBar';
-import { useTeamPageContext } from '../../context/teamPageContext';
+import {useTeamPageContext} from '../../context/teamPageContext';
 import LiveEvents from '../common/LiveEvents';
 import Standings from '../common/Standings';
 import TeamNews from '../common/TeamNews';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 const teamUpcomingEvents = [
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
-    { id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12' },
+    {id: 1, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12'},
+    {id: 2, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12'},
+    {id: 3, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12'},
+    {id: 4, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12'},
+    {id: 5, homeTeam: 'Liverpool', awayTeam: 'Chelsea', time: '21:00', date: '2022-12-12'},
 ];
 
 const teamLiveEvents = [
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00', homeTeamGoals: 2, awayTeamGoals: 1 },
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00', homeTeamGoals: 2, awayTeamGoals: 1 },
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00', homeTeamGoals: 2, awayTeamGoals: 1 },
-    { id: 1, homeTeam: 'Liverpool', awayTeam: 'Arsenal', scoreHomeTeam: 2, scoreAwayTeam: 1, time: '90:00', homeTeamGoals: 2, awayTeamGoals: 1 },
+    {
+        id: 1,
+        homeTeam: 'Liverpool',
+        awayTeam: 'Arsenal',
+        scoreHomeTeam: 2,
+        scoreAwayTeam: 1,
+        time: '90:00',
+        homeTeamGoals: 2,
+        awayTeamGoals: 1
+    },
+    {
+        id: 1,
+        homeTeam: 'Liverpool',
+        awayTeam: 'Arsenal',
+        scoreHomeTeam: 2,
+        scoreAwayTeam: 1,
+        time: '90:00',
+        homeTeamGoals: 2,
+        awayTeamGoals: 1
+    },
+    {
+        id: 1,
+        homeTeam: 'Liverpool',
+        awayTeam: 'Arsenal',
+        scoreHomeTeam: 2,
+        scoreAwayTeam: 1,
+        time: '90:00',
+        homeTeamGoals: 2,
+        awayTeamGoals: 1
+    },
+    {
+        id: 1,
+        homeTeam: 'Liverpool',
+        awayTeam: 'Arsenal',
+        scoreHomeTeam: 2,
+        scoreAwayTeam: 1,
+        time: '90:00',
+        homeTeamGoals: 2,
+        awayTeamGoals: 1
+    },
 ];
 
 const TeamPage = (): JSX.Element => {
     const isMobile = useMediaQuery('(max-width:600px)');
     const location = useLocation();
-    const { selectedButton, setSelectedButton } = useTeamPageContext();
+    const {selectedButton, setSelectedButton} = useTeamPageContext();
 
     useEffect(() => {
         setSelectedButton(0);
@@ -35,22 +71,53 @@ const TeamPage = (): JSX.Element => {
 
     const renderContent = () => {
         switch (selectedButton) {
-            case 0: return <TeamNews />;
-            case 1: return <LiveEvents events={teamLiveEvents} page="team" />;
-            case 2: return <Standings page="team" />;
-            case 3: return <UpcomingEvents events={teamUpcomingEvents} page='team' />;
-            default: return null;
+            case 0:
+                return <TeamNews/>;
+            case 1:
+                return <LiveEvents events={teamLiveEvents} page="team"/>;
+            case 2:
+                return <Standings page="team"/>;
+            case 3:
+                return <UpcomingEvents events={teamUpcomingEvents} page='team'/>;
+            default:
+                return null;
         }
     }
 
     return (
-        <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', padding: '0' }}>
-            <TeamBanner />
-            <TeamPageBar />
-            <Box sx={{ flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                {renderContent()}
-            </Box>
-        </Container>
+        <>
+            {isMobile ?
+                <Container maxWidth="xl" sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: '10px',
+                    padding: '0'
+                }}>
+                    <TeamBanner/>
+                    <TeamPageBar/>
+                    <Box
+                        sx={{flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                        {renderContent()}
+                    </Box>
+                </Container>
+                :
+                <Container maxWidth="xl" sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: '10px',
+                    padding: '0'
+                }}>
+                    <TeamBanner/>
+                    <TeamPageBar/>
+                    <Box
+                        sx={{flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                        {renderContent()}
+                    </Box>
+                </Container>
+            }
+        </>
     )
 }
 
