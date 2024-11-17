15d706ca3ac42110f7b4bfa623e6a526da265e56
Angel <angel.pashov@proton.me>
2024-11-17 16:50:23 +0200

    Created the news page component

---
+++ src/components/pages/NewsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsPage: React.FC = (): JSX.Element => {
+    return (
+        <div>
+            <h1>News Page</h1>
+        </div>
+    );
+};
+
+export default NewsPage;
