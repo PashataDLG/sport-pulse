1686c95cba8b9580902ff1b8ec7498dd8ac389d3
Angel <angel.pashov@proton.me>
2024-11-17 13:27:58 +0200

    update theme with custom breakpoints

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,27 @@
-import {createTheme} from '@mui/material/styles';
+import {createTheme, Theme} from '@mui/material/styles';
 
-const theme = createTheme({
+declare module '@mui/material/styles' {
+    interface BreakpointOverrides {
+        xs: true;
+        sm: true;
+        md: true;
+        lg: true;
+        xl: true;
+        iphoneSE: true;
+        iphone14ProMax: true;
+    }
+}
+
+const theme: Theme = createTheme({
     breakpoints: {
         values: {
             xs: 0,    
             sm: 600,     
             md: 768,      
             lg: 1024,
-            xl: 1440   
+            xl: 1440,
+            iphoneSE: 375,
+            iphone14ProMax: 430,
         }
     },
 });
