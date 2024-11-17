f2b06fc2b7fefa054809fb8639a76fd458b953ab
Angel <angel.pashov@proton.me>
2024-11-17 17:23:38 +0200

    Added the route for news page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Header from "./components/layout/Header"
 import Home from "./components/pages/Home"
 import TeamPage from './components/pages/TeamPage';
+import NewsPage from "./components/pages/NewsPage.tsx";
 
 function App() {
 
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/team-details' element ={<TeamPage />} />
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/team-details' element={<TeamPage/>}/>
+                <Route path='/news' element={<NewsPage/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
