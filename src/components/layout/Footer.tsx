13e99d92b243b0ff0f58fdd814e71bbe27c0eb05
Angel <angel.pashov@proton.me>
2024-11-18 17:11:51 +0200

    Added social media icons to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,9 @@
-import { Box } from "@mui/material";
+import { FaFacebook } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTiktok } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { Box, IconButton } from "@mui/material";
 
 const Footer: React.FC = (): JSX.Element => {
     return (
@@ -8,8 +13,25 @@ const Footer: React.FC = (): JSX.Element => {
             justifyContent: 'center',
             backgroundColor: '#242424',
             width: '100%',
+            height: '30%',
         }}>
-            
+            <Box sx={{ display: 'flex', gap: '15px', marginBottom: '10px' }}>
+                <IconButton href="https://www.facebook.com" target="_blank" sx={{ color: 'white' }}>
+                    <FaFacebook size={24} />
+                </IconButton>
+                <IconButton href="https://www.twitter.com" target="_blank" sx={{ color: 'white' }}>
+                    <RiTwitterXLine size={24} />
+                </IconButton>
+                <IconButton href="https://www.instagram.com" target="_blank" sx={{ color: 'white' }}>
+                    <AiFillInstagram size={24} />
+                </IconButton>
+                <IconButton href="https://www.linkedin.com" target="_blank" sx={{ color: 'white' }}>
+                    <FaTiktok size={24} />
+                </IconButton>
+                <IconButton href="https://www.linkedin.com" target="_blank" sx={{ color: 'white' }}>
+                    <FaYoutube size={24} />
+                </IconButton>
+            </Box>
         </Box>
     );
 };
