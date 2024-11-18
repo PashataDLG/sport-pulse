910e0ab0d7ecf554cb555846ec148a3d6f6ef6af
Angel <angel.pashov@proton.me>
2024-11-18 15:02:08 +0200

    team news on click leads to news page.

--- src/components/common/TeamNews.tsx
+++ src/components/common/TeamNews.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { FaRegNewspaper } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const TeamNews = () => {
     return (
@@ -10,21 +11,21 @@ const TeamNews = () => {
                 Team News
             </Typography>
             {/* News box */}
-            <Box sx={{ 
-                display: 'flex', 
-                flexDirection: {xs: 'column', sm: 'column', md: 'column', lg: 'row'},
-                gap: '40px', 
+            <Box sx={{
+                display: 'flex',
+                flexDirection: { xs: 'column', sm: 'column', md: 'column', lg: 'row' },
+                gap: '40px',
                 width: '100%',
-                justifyContent: 'flex-start', 
-                alignItems: 'center', 
-                flexWrap: 'wrap', 
-                alignContent: 'flex-start', 
-                marginTop: '20px' 
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+                alignContent: 'flex-start',
+                marginTop: '20px'
             }}>
                 {/* News container */}
                 <Box
                     sx={{
-                        width: {xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%'},
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
                         height: '100%',
                         display: 'flex',
                         flexDirection: 'column',
@@ -35,6 +36,7 @@ const TeamNews = () => {
                             cursor: 'pointer',
                         },
                     }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
                     <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
                     <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
                         Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
@@ -42,10 +44,11 @@ const TeamNews = () => {
                     <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
                         12.06.24
                     </Typography>
-                </Box>
-                <Box
+                </Link>
+            </Box>
+            <Box
                     sx={{
-                        width: {xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%'},
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
                         height: '100%',
                         display: 'flex',
                         flexDirection: 'column',
@@ -56,6 +59,7 @@ const TeamNews = () => {
                             cursor: 'pointer',
                         },
                     }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
                     <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
                     <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
                         Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
@@ -63,10 +67,11 @@ const TeamNews = () => {
                     <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
                         12.06.24
                     </Typography>
-                </Box>
-                <Box
+                </Link>
+            </Box>
+            <Box
                     sx={{
-                        width: {xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%'},
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
                         height: '100%',
                         display: 'flex',
                         flexDirection: 'column',
@@ -77,6 +82,7 @@ const TeamNews = () => {
                             cursor: 'pointer',
                         },
                     }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
                     <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
                     <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
                         Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
@@ -84,10 +90,11 @@ const TeamNews = () => {
                     <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
                         12.06.24
                     </Typography>
-                </Box>
-                <Box
+                </Link>
+            </Box>
+            <Box
                     sx={{
-                        width: {xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%'},
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
                         height: '100%',
                         display: 'flex',
                         flexDirection: 'column',
@@ -98,6 +105,7 @@ const TeamNews = () => {
                             cursor: 'pointer',
                         },
                     }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
                     <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
                     <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
                         Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
@@ -105,9 +113,56 @@ const TeamNews = () => {
                     <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
                         12.06.24
                     </Typography>
-                </Box>
+                </Link>
+            </Box>
+            <Box
+                    sx={{
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        '&:hover .news-title': {
+                            color: '#1976d2 !important',
+                        },
+                        '&:hover': {
+                            cursor: 'pointer',
+                        },
+                    }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
+                    <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
+                    <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
+                        Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
+                    </Typography>
+                    <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
+                        12.06.24
+                    </Typography>
+                </Link>
+            </Box>
+            <Box
+                    sx={{
+                        width: { xs: '100%', sm: '100%', md: '100%', lg: '30%', xl: '28%' },
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        '&:hover .news-title': {
+                            color: '#1976d2 !important',
+                        },
+                        '&:hover': {
+                            cursor: 'pointer',
+                        },
+                    }}>
+                    <Link to="/news" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column'}}>
+                    <Box component="img" src='https://sportal365images.com/process/smp-images-production/sportal.bg/19052024/ac94e4b8-4b95-4101-b903-c96fb5f57682.jpg?operations=crop(80:551:5663:3692),fit(968:545)&format=webp' />
+                    <Typography component="h2" className="news-title" sx={{ paddingTop: '10px', fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '1.2rem' }}>
+                        Liverpool, Man United, Arsenal and Tottenham, will seek compensation from Man City, if the 'citizens' are found guilty
+                    </Typography>
+                    <Typography sx={{ paddingTop: '12px', fontFamily: 'Roboto Mono' }}>
+                        12.06.24
+                    </Typography>
+                </Link>
             </Box>
         </Box>
+        </Box >
     )
 }
 
