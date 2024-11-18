4217aedd7495b919b3b8a1d5fe3370e7017ea7c8
Angel <angel.pashov@proton.me>
2024-11-18 17:08:51 +0200

    Created the footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+const Footer: React.FC = (): JSX.Element => {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: '#242424',
+            width: '100%',
+        }}>
+            
+        </Box>
+    );
+};
+
+export default Footer;
