1d2ab5c125531e8dc49cae478b6d53ba9e10f6fd
Angel <angel.pashov@proton.me>
2024-11-24 11:44:37 +0200

    updated the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import standingsReducer from './slices/standingsSlice';
 
 const store = configureStore({
     reducer: {
-        // Add reducers here
+        standings: standingsReducer,
     }
 });
 
