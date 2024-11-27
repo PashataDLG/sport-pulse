9662a3574e3b949b60fc8f542e30b2c2c44b55a7
Angel <angel.pashov@proton.me>
2024-11-27 17:39:12 +0200

    Added the queryclient and provider to the application main file

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,13 @@ import { ThemeProvider } from '@mui/material/styles'
 import {AppContextProvider} from './context/appContext.tsx'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { TeamPageProvider } from './context/teamPageContext.tsx'
+import { QueryClient, QueryClientProvider  } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
+		<QueryClientProvider client={queryClient}>
 		<Router>
 			<TeamPageProvider>
 				<AppContextProvider>
@@ -19,5 +23,6 @@ createRoot(document.getElementById('root')!).render(
 				</AppContextProvider>
 			</TeamPageProvider>
 		</Router>
+		</QueryClientProvider>
 	</StrictMode>
 );
