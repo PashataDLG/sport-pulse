3937bcd54e0b3817306be3d6826fb2e7cb6d6f99
Angel <angel.pashov@proton.me>
2024-11-28 23:34:22 +0200

    useStandingsData.ts custom hook now uses useEffect to save changes in the state

--- src/hooks/useStandingsData.ts
+++ src/hooks/useStandingsData.ts
@@ -3,21 +3,27 @@ import {useDispatch} from 'react-redux';
 import {fetchStandingsData} from '../api/standingsApi';
 import {fetchStandingsSuccess, fetchStandingsFailure} from '../store/slices/standingsSlice';
 import type {ApiResponse} from '../store/slices/standingsSlice';
+import {Dispatch, useEffect, useState} from 'react';
+import {UnknownAction} from "@reduxjs/toolkit";
 
 export const useStandingsData = () => {
-    const dispatch = useDispatch();
+    const dispatch: Dispatch<UnknownAction> = useDispatch();
+    const [enabled, setEnabled] = useState(true);
 
     const {data, error, isError, isSuccess} = useQuery<ApiResponse, Error>({
         queryKey: ['standings'],
         queryFn: fetchStandingsData,
-        enabled: true,
+        enabled,
     });
 
-    if (isSuccess && data) {
-        dispatch(fetchStandingsSuccess(data));
-    } else if (isError && error) {
-        dispatch(fetchStandingsFailure((error as Error).message));
-    }
+    useEffect((): void => {
+        if (isSuccess && data) {
+            dispatch(fetchStandingsSuccess(data));
+            setEnabled(false);
+        } else if (isError && error) {
+            dispatch(fetchStandingsFailure((error as Error).message));
+        }
+    }, [isSuccess, isError]);
 
-    return {data, error};
+    return {data, error, isError, isSuccess};
 };
