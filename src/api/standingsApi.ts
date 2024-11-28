b81fb4dccfe697fb8f7b56d69ce3252dc07f903b
Angel <angel.pashov@proton.me>
2024-11-28 23:29:34 +0200

    Updated the standingsApi.ts

--- src/api/standingsApi.ts
+++ src/api/standingsApi.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import type { ApiResponse } from '../store/slices/standingsSlice';
 
-export const fetchStandingsData = async (): Promise<ApiResponse> => {
-    const response = await axios.get<ApiResponse>(`https://api.football-data.org/v4/competitions/PL/standings?season=2024`, {
-        headers: {
-            'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
-        },
-    });
-
+export const fetchStandingsData = async ()=> {
+    const response = await axios.get(
+        'https://api.football-data.org/v4/competitions/PL/standings?season=2024',
+        {
+            headers: {
+                'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
+            },
+        }
+    );
     return response.data;
 };
