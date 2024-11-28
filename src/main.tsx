29e9c690c302c2d068cf29f2ae35227495800def
Angel <angel.pashov@proton.me>
2024-11-28 23:29:11 +0200

    Added the store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import theme from './theme/theme.ts'
-import { ThemeProvider } from '@mui/material/styles'
+import {ThemeProvider} from '@mui/material/styles'
 import {AppContextProvider} from './context/appContext.tsx'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { TeamPageProvider } from './context/teamPageContext.tsx'
-import { QueryClient, QueryClientProvider  } from '@tanstack/react-query'
+import {BrowserRouter as Router} from 'react-router-dom';
+import {TeamPageProvider} from './context/teamPageContext.tsx'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {Provider} from 'react-redux';
+import store from "./store/store.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-		<Router>
-			<TeamPageProvider>
-				<AppContextProvider>
-					<ThemeProvider theme={theme}>
-						<App />
-					</ThemeProvider>
-				</AppContextProvider>
-			</TeamPageProvider>
-		</Router>
-		</QueryClientProvider>
-	</StrictMode>
+    <StrictMode>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <TeamPageProvider>
+                        <AppContextProvider>
+                            <ThemeProvider theme={theme}>
+                                <App/>
+                            </ThemeProvider>
+                        </AppContextProvider>
+                    </TeamPageProvider>
+                </Router>
+            </QueryClientProvider>
+        </Provider>
+    </StrictMode>
 );
