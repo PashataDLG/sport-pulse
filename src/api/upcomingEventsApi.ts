de5c52ed5873f95eb4ae45ec89093e6bf4b5413e
Angel <angel.pashov@proton.me>
2024-12-03 13:51:49 +0200

    upcomingEventsApi.ts now uses today's date to make the api call

--- src/api/upcomingEventsApi.ts
+++ src/api/upcomingEventsApi.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { getTodayDate} from "../utils/getTodayDate.ts";
 
-export const fetchUpcomingEvents = async ()=> {
+export const fetchUpcomingEvents = async () => {
+    const todayDate: string = getTodayDate();
+
     const response = await axios.get(
-        'https://api.football-data.org/v4/competitions/PL/standings?season=2024',
+        `https://api.football-data.org/v4/competitions/PL/matches?dateFrom=${todayDate}&dateTo=${todayDate}`,
         {
             headers: {
                 'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
