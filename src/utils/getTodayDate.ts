ba52c04c2bc4578b7926f0b090c8af558ab9d640
Angel <angel.pashov@proton.me>
2024-12-05 18:46:56 +0200

    Fixed get today date instead of getting day

--- src/utils/getTodayDate.ts
+++ src/utils/getTodayDate.ts
@@ -3,7 +3,7 @@ export const getTodayDate = (): string => {
 
     const year: number = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDay()).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
 
     return `${year}-${month}-${day}`;
 };
