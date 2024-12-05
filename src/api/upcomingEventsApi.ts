752d4cf2f184f2c563f4aabd3a2cb2698606f760
Angel <angel.pashov@proton.me>
2024-12-05 19:46:04 +0200

    upcoming events api uses the new get date function

--- src/api/upcomingEventsApi.ts
+++ src/api/upcomingEventsApi.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { getTodayDate} from "../utils/getTodayDate.ts";
 
 export const fetchUpcomingEvents = async () => {
-    const todayDate: string = getTodayDate();
+    const { apiDate } = getTodayDate();
 
     const response = await axios.get(
-        `https://api.football-data.org/v4/competitions/PL/matches?dateFrom=${todayDate}&dateTo=${todayDate}`,
+        `https://api.football-data.org/v4/competitions/PL/matches?dateFrom=${apiDate}&dateTo=${apiDate}`,
         {
             headers: {
                 'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
