5c084f70b87e31127c72616992c79ab7415439b9
Angel <angel.pashov@proton.me>
2024-12-05 19:45:10 +0200

    Refactored getTodayDate.ts so we can get the display and api date

--- src/utils/getTodayDate.ts
+++ src/utils/getTodayDate.ts
@@ -1,9 +1,12 @@
-export const getTodayDate = (): string => {
+export const getTodayDate= () : {apiDate: string, displayDate: string} => {
     const today = new Date();
 
     const year: number = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const day = String(today.getDate()).padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
+    const apiDate = `${year}-${month}-${day}`;
+    const displayDate = `${day}.${month}.${String(year).slice(-2)}`;
+
+    return { apiDate, displayDate };
 };
