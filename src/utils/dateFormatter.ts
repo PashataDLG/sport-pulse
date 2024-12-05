53fb3ccac58efc5a324abdf172c81bf60c25829c
Angel <angel.pashov@proton.me>
2024-12-05 19:44:36 +0200

    Added date formatter

--- src/utils/dateFormatter.ts
+++
@@ -1,8 +0,0 @@
-const dateFormatter = (date: string): string => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    const formattedDate: string = new Date(date).toLocaleDateString('en-US', options);
-    const [year, month, day] = formattedDate.split('/');
-    return `${year}.${month}.${day}`;
-};
-
-export default dateFormatter;
---
+++ src/utils/timeFormatter.ts
@@ -0,0 +1,6 @@
+export const timeFormatter = (isoString: string): string => {
+    const date = new Date(isoString);
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+};
