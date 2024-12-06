477eef8ac287318a31f2a951c7aa7190ae5d53cd
Angel <angel.pashov@proton.me>
2024-12-06 18:31:56 +0200

    changed the name arg name from isoString to utcDate for clarity

--- src/utils/timeFormatter.ts
+++ src/utils/timeFormatter.ts
@@ -1,5 +1,5 @@
-export const timeFormatter = (isoString: string): string => {
-    const date = new Date(isoString);
+export const timeFormatter = (utcDate: string): string => {
+    const date = new Date(utcDate);
     const hours = String(date.getUTCHours()).padStart(2, '0');
     const minutes = String(date.getUTCMinutes()).padStart(2, '0');
     return `${hours}:${minutes}`;
