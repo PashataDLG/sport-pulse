b3ee384384dc271a6393de461a3661063f1801fe
Angel <angel.pashov@proton.me>
2024-12-06 18:34:46 +0200

    Fixed the width of the styled components

--- src/components/common/Styledbox.tsx
+++ src/components/common/Styledbox.tsx
@@ -1,13 +1,12 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/system';
-import { Theme } from '@mui/material/styles';
 
 interface StyledBoxProps {
     page: 'team' | 'home';
 }
 
 const StyledBox = styled(Box)<StyledBoxProps>(({ theme, page }) => ({
-	width: page === 'team' ? '89%' : '26.5%',
+	width: page === 'team' ? '89%' : '32.5%',
 	gap: theme.spacing(2),
 	[theme.breakpoints.down('md')]: {
 		width: '100%',
