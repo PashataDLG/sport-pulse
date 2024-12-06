6de51938948cabf4498882f1b7c6517dccb53313
Angel <angel.pashov@proton.me>
2024-12-06 18:36:34 +0200

    Fixed the layout of the UpcomingEvents.tsx, and the crest are using img tag instead of the circular avater

--- src/components/common/UpcomingEvents.tsx
+++ src/components/common/UpcomingEvents.tsx
@@ -1,112 +1,64 @@
 import React from 'react';
-import { Box, Typography, Avatar, Paper, Table, TableBody, TableCell, TableContainer, TableRow, useMediaQuery,} from '@mui/material';
+import { Box, Typography, Avatar, Paper, Table, TableBody, TableCell, TableContainer, TableRow, useMediaQuery } from '@mui/material';
 import { MdOutlineNextWeek } from "react-icons/md";
-import {timeFormatter} from "../../utils/timeFormatter.ts";
+import { timeFormatter } from "../../utils/timeFormatter.ts";
 import theme from '../../theme/theme';
 import StyledBox from './Styledbox';
-import { useUpcomingEventsData} from "../../hooks/useUpcomingEventsData.ts";
-import {Match} from "../../store/slices/upcomingEventsSlice.ts";
-import {useSelector} from "react-redux";
-import {RootState} from '../../store/store';
-import {getTodayDate} from "../../utils/getTodayDate.ts";
-
-interface Event {
-	id: number;
-	homeTeam: string;
-	awayTeam: string;
-	time: string;
-	date: string;
-}
+import { useUpcomingEventsData } from "../../hooks/useUpcomingEventsData.ts";
+import { Match } from "../../store/slices/upcomingEventsSlice.ts";
+import { useSelector } from "react-redux";
+import { RootState } from '../../store/store';
+import { dateFormatter } from "../../utils/dateFormatter.ts";
 
 interface UpcomingEventsProps {
-	events: Event[];
 	page: 'team' | 'home';
 }
 
-const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events, page }) => {
+const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ page }) => {
 	useUpcomingEventsData();
 	const matches: Match[] = useSelector((state: RootState): Match[] => state.upcomingEvents.matches);
 
-	const { displayDate } = getTodayDate();
-
-	const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+	const isMobile: boolean = useMediaQuery(theme.breakpoints.down('lg'));
 
 	return (
 		<>
 			{isMobile ? (
-				<Box sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					bgcolor: 'background.paper',
-					width: '100%'
-				}}>
-					<Typography variant="h4" gutterBottom sx={{
-						fontFamily: 'Montserrat',
-						fontWeight: 'bold',
-						textAlign: 'center',
-						color: '#1976d2',
-						borderRadius: '20px',
-						marginBottom: '20px',
-						marginTop: '0px',
-						width: '100%',
-						alignSelf: 'center',
-						fontSize: '2rem',
-					}}>
+				<Box sx={{ display: 'flex', flexDirection: 'column', bgcolor: 'background.paper', width: '100%' }}>
+					<Typography variant="h4" gutterBottom sx={{ fontFamily: 'Montserrat', fontWeight: 'bold', textAlign: 'center', color: '#1976d2', borderRadius: '20px', marginBottom: '20px', marginTop: '0px', width: '100%', alignSelf: 'center', fontSize: '2rem' }}>
 						<MdOutlineNextWeek size={30} style={{ color: '#1976d2', marginRight: '10px' }} />
 						Upcoming Events
 					</Typography>
-					<TableContainer component={Paper} sx={{
-						maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px'
-					}}>
+					<TableContainer component={Paper} sx={{ maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px' }}>
 						<Table stickyHeader>
-
 							<TableBody>
-								{events.map((event) => (
-									<TableRow key={event.id}>
+								{matches.map((match: Match) => (
+									<TableRow key={match.id}>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												alignItems: 'center',
-												gap: '5px'
-											}}>
-												<Avatar src='https://www.thesportsdb.com/images/media/team/badge/ar5tn91728915882.png' />{event.homeTeam}
+											<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '5px' }}>
+												<Avatar src={match.homeTeam.crest} />
+												<Typography sx={{ fontFamily: 'Poppins', maxWidth: '150px', textAlign: 'center' }}>
+													{match.homeTeam.shortName}
+												</Typography>
 											</Box>
 										</TableCell>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												alignItems: 'center',
-												gap: '5px',
-												padding: '20px',
-												marginTop: '-30px'
-											}}>
+											<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px', padding: '20px', marginTop: '-30px' }}>
 												<Typography sx={{ fontSize: '0.9rem', color: 'text.secondary', fontFamily: 'Poppins' }}>
-													{displayDate}
+													{dateFormatter(match.utcDate)}
 												</Typography>
-												<Box sx={{
-													display: 'flex',
-													justifyContent: 'center',
-													alignItems: 'center',
-													gap: '10px',
-												}}>
+												<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
 													<Typography sx={{ fontSize: '1.1rem', fontFamily: 'Poppins' }}>
-														{event.time}
+														{timeFormatter(match.utcDate)}
 													</Typography>
 												</Box>
 											</Box>
 										</TableCell>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												alignItems: 'center',
-												gap: '5px',
-											}}>
-												<Avatar src='https://www.thesportsdb.com/images/media/team/badge/yvwvtu1448813215.png' />{event.awayTeam}
+											<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '5px' }}>
+												<Avatar src={match.awayTeam.crest} />
+												<Typography sx={{ fontFamily: 'Poppins', maxWidth: '150px', textAlign: 'center' }}>
+													{match.awayTeam.shortName}
+												</Typography>
 											</Box>
 										</TableCell>
 									</TableRow>
@@ -116,65 +68,30 @@ const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events, page }) => {
 					</TableContainer>
 				</Box>
 			) : (
-				<StyledBox page={page} sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					bgcolor: 'background.paper',
-
-				}}>
-					<Typography variant="h4" gutterBottom sx={{
-						fontFamily: 'Montserrat',
-						fontWeight: 'bold',
-						color: '#1976d2',
-						textAlign: 'center',
-						borderRadius: '20px',
-						marginBottom: '20px',
-						marginTop: '0px',
-						width: '100%',
-						alignSelf: 'center',
-						fontSize: '2rem',
-					}}>
+				<StyledBox page={page} sx={{ display: 'flex', flexDirection: 'column', bgcolor: 'background.paper' }}>
+					<Typography variant="h4" gutterBottom sx={{ fontFamily: 'Montserrat', fontWeight: 'bold', color: '#1976d2', textAlign: 'center', borderRadius: '20px', marginBottom: '20px', marginTop: '0px', width: '100%', alignSelf: 'center', fontSize: '2rem' }}>
 						<MdOutlineNextWeek size={30} style={{ color: '#1976d2', marginRight: '10px' }} />
 						Upcoming Events
 					</Typography>
-					<TableContainer component={Paper} sx={{
-						maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px'
-					}}>
+					<TableContainer component={Paper} sx={{ maxHeight: 500, maxWidth: '100%', overflow: 'auto', borderRadius: '12px' }}>
 						<Table stickyHeader>
-
 							<TableBody>
-								{matches.map((match) => (
+								{matches.map((match: Match) => (
 									<TableRow key={match.id}>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												alignItems: 'center',
-												gap: '5px'
-											}}>
-												<Avatar src={match.homeTeam.crest}/>
-												<Typography sx={{ fontFamily: 'Poppins' }}>{match.homeTeam.shortName}</Typography>
+											<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+												<Box component='img' src={match.homeTeam.crest} sx={{ width: '35px '}} />
+												<Typography sx={{ fontFamily: 'Poppins', maxWidth: '150px', textAlign: 'center' }}>
+													{match.homeTeam.shortName}
+												</Typography>
 											</Box>
 										</TableCell>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												alignItems: 'center',
-												gap: '5px',
-												padding: '20px',
-												marginTop: '-30px'
-											}}>
+											<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px', padding: '20px', marginTop: '-30px' }}>
 												<Typography sx={{ fontSize: '0.9rem', color: 'text.secondary', fontFamily: 'Roboto Mono' }}>
-													{displayDate}
+													{dateFormatter(match.utcDate)}
 												</Typography>
-												<Box sx={{
-													display: 'flex',
-													justifyContent: 'center',
-													alignItems: 'center',
-													gap: '10px',
-												}}>
+												<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
 													<Typography sx={{ fontSize: '1.1rem', fontFamily: 'Roboto Mono' }}>
 														{timeFormatter(match.utcDate)}
 													</Typography>
@@ -182,15 +99,11 @@ const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events, page }) => {
 											</Box>
 										</TableCell>
 										<TableCell>
-											<Box sx={{
-												display: 'flex',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												alignItems: 'center',
-												gap: '5px',
-											}}>
-												<Avatar src={match.awayTeam.crest} />
-												<Typography sx={{ fontFamily: 'Poppins' }}>{match.awayTeam.shortName}</Typography>
+											<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '5px' }}>
+												<Box component='img' src={match.awayTeam.crest} sx={{ width: '35px '}} />
+												<Typography sx={{ fontFamily: 'Poppins', maxWidth: '150px', textAlign: 'center' }}>
+													{match.awayTeam.shortName}
+												</Typography>
 											</Box>
 										</TableCell>
 									</TableRow>
