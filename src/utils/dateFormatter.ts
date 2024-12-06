a174df7e799cf45170bb49e7f724894e5cec8850
Angel <angel.pashov@proton.me>
2024-12-06 18:31:14 +0200

    Created dateFormatter.ts, formats the date from UTC format

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,7 @@
+export const dateFormatter = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = String(date.getUTCFullYear()).slice(-2);
+    return `${day}.${month}.${year}`;
+};
