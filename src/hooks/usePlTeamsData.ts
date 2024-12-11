4fc890850bdd7acd5a88c7f1c262ed67a6c46d03
Angel <angel.pashov@proton.me>
2024-12-11 18:57:03 +0200

    Created the hook for the pl teams

---
+++ src/hooks/usePlTeamsData.ts
@@ -0,0 +1,28 @@
+import { fetchPlTeams} from "../api/plTeamsApi.ts";
+import {Dispatch, useEffect, useState} from "react";
+import {useDispatch} from 'react-redux';
+import {fetchPlTeamsSuccess, TeamsResponse} from "../store/slices/plTeamsSlice";
+import {useQuery} from "@tanstack/react-query";
+import {UnknownAction} from "@reduxjs/toolkit";
+
+export const usePlTeamsData = () => {
+    const dispatch: Dispatch<UnknownAction> = useDispatch();
+    const [enabled, setEnabled] = useState<boolean>(true);
+
+    const {data, error, isError, isSuccess} = useQuery<TeamsResponse, Error>({
+        queryKey: ['plTeams'],
+        queryFn: fetchPlTeams,
+        enabled,
+    });
+
+    useEffect(() => {
+        if(isSuccess && data) {
+            dispatch(fetchPlTeamsSuccess(data));
+            setEnabled(false);
+        } else if(isError && error) {
+            console.error((error as Error).message);
+        }
+    }, [data, error, isError, isSuccess]);
+
+    return { data };
+};
