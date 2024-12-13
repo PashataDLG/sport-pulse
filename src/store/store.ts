851a18b26d249fed5eef33086dcc6e9ab8c6c398
Angel <angel.pashov@proton.me>
2024-12-13 16:01:07 +0200

    added player slice to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,15 @@ import standingsReducer from './slices/standingsSlice';
 import upcomingEventsReducer from './slices/upcomingEventsSlice';
 import liveEventsReducer from './slices/liveEventsSlice';
 import plTeamsReducer from './slices/plTeamsSlice';
+import playersReducer from './slices/playersSlice';
 
 const store = configureStore({
     reducer: {
         standings: standingsReducer,
         upcomingEvents: upcomingEventsReducer,
         liveEvents: liveEventsReducer,
-        plTeams: plTeamsReducer
+        plTeams: plTeamsReducer,
+        plPlayers: playersReducer,
     }
 });
 
