ba05c74fb1cdb603e03cba40b6390a01009c0231
Angel <angel.pashov@proton.me>
2024-12-13 16:01:48 +0200

    players api created

---
+++ src/api/plPlayersApi.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance } from 'axios';
+import { TeamPlayersResponse } from '../store/slices/playersSlice';
+
+const playersApiClient: AxiosInstance = axios.create({
+    baseURL: `https://api.football-data.org/v4/teams`,
+    headers: {
+        'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
+    },
+});
+
+export const fetchPlayersData: (teamId: number) => Promise<TeamPlayersResponse> = async (teamId: number) => {
+    try {
+        const response = await playersApiClient.get(`/${teamId}`);
+
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
