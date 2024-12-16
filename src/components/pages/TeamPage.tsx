64f4fb6404cfb034de24f2ca8cf5050cd2592d48
Angel <angel.pashov@proton.me>
2024-12-16 16:20:36 +0200

    When squad is clicked, the component renders

--- src/components/pages/TeamPage.tsx
+++ src/components/pages/TeamPage.tsx
@@ -1,23 +1,28 @@
-import {Box, Container, useMediaQuery} from '@mui/material'
+import { Box, Container, useMediaQuery } from '@mui/material'
+
 import TeamBanner from '../common/TeamBanner';
 import UpcomingEvents from '../common/UpcomingEvents';
 import TeamPageBar from '../common/TeamPageBar';
-import {useTeamPageContext} from '../../context/teamPageContext';
-import LiveEvents from '../common/LiveEvents';
+import Squad from '../common/Squad.tsx';
 import Standings from '../common/Standings';
 import TeamNews from '../common/TeamNews';
-import {useEffect} from 'react';
-import {useLocation, useParams} from 'react-router-dom';
-import {Team, TeamsResponse} from "../../store/slices/plTeamsSlice.ts";
-import {useSelector} from "react-redux";
-import {RootState} from "../../store/store.ts";
+
+import { useTeamPageContext } from '../../context/teamPageContext';
+import { useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Team, TeamsResponse } from "../../store/slices/plTeamsSlice.ts";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store.ts";
 
 const TeamPage = (): JSX.Element => {
     const isMobile = useMediaQuery('(max-width:600px)');
     const location = useLocation();
-    const {selectedButton, setSelectedButton} = useTeamPageContext();
-    const { teamId } = useParams<{teamId: string}>();
+
+    const { selectedButton, setSelectedButton } = useTeamPageContext();
+    const { teamId } = useParams<{ teamId: string }>();
+
     const plTeams: TeamsResponse = useSelector((state: RootState): TeamsResponse => state.plTeams);
+
     const team: Team = plTeams.teams.find((team: Team) => Number(team.idTeam) === parseInt(teamId ?? '0')) as Team;
 
     useEffect(() => {
@@ -27,13 +32,13 @@ const TeamPage = (): JSX.Element => {
     const renderContent = () => {
         switch (selectedButton) {
             case 0:
-                return <TeamNews/>;
+                return <TeamNews />;
             case 1:
-                return <LiveEvents page="team"/>;
+                return <Squad />;
             case 2:
-                return <UpcomingEvents page='team'/>;
+                return <UpcomingEvents page='team' />;
             case 3:
-                return <Standings page="team"/>;
+                return <Standings page="team" />;
             default:
                 return null;
         }
@@ -49,10 +54,10 @@ const TeamPage = (): JSX.Element => {
                     gap: '10px',
                     padding: '0'
                 }}>
-                    <TeamBanner team={team}/>
-                    <TeamPageBar/>
+                    <TeamBanner team={team} />
+                    <TeamPageBar />
                     <Box
-                        sx={{flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                        sx={{ flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
                         {renderContent()}
                     </Box>
                 </Container>
@@ -64,10 +69,10 @@ const TeamPage = (): JSX.Element => {
                     gap: '10px',
                     padding: '0'
                 }}>
-                    <TeamBanner team={team}/>
-                    <TeamPageBar/>
+                    <TeamBanner team={team} />
+                    <TeamPageBar />
                     <Box
-                        sx={{flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                        sx={{ flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
                         {renderContent()}
                     </Box>
                 </Container>
