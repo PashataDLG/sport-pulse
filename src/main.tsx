26144df124d04008c8e558e1e5b42a2d1b4cc06e
Angel <angel.pashov@proton.me>
2024-12-29 16:42:23 +0200

    auth0provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,43 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.tsx'
 import './index.css'
 import theme from './theme/theme.ts'
-import {ThemeProvider} from '@mui/material/styles'
-import {AppContextProvider} from './context/appContext.tsx'
-import {BrowserRouter as Router} from 'react-router-dom';
-import {TeamPageProvider} from './context/teamPageContext.tsx'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {Provider} from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles'
+import { AppContextProvider } from './context/appContext.tsx'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { TeamPageProvider } from './context/teamPageContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux';
 import store from "./store/store.ts";
 
 const queryClient = new QueryClient();
 
+const domain: string = process.env.REACT_APP_AUTH0_DOMAIN as string;
+const clientId: string = process.env.REACT_APP_AUTH0_CLIENT_ID as string;
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <TeamPageProvider>
-                        <AppContextProvider>
-                            <ThemeProvider theme={theme}>
-                                <App/>
-                            </ThemeProvider>
-                        </AppContextProvider>
-                    </TeamPageProvider>
-                </Router>
-            </QueryClientProvider>
+            <Auth0Provider 
+                domain={domain}
+                clientId={clientId}
+                authorizationParams={{
+                    redirect_uri: window.location.origin,
+                  }}>
+                <QueryClientProvider client={queryClient}>
+                    <Router>
+                        <TeamPageProvider>
+                            <AppContextProvider>
+                                <ThemeProvider theme={theme}>
+                                    <App />
+                                </ThemeProvider>
+                            </AppContextProvider>
+                        </TeamPageProvider>
+                    </Router>
+                </QueryClientProvider>
+            </Auth0Provider>
         </Provider>
     </StrictMode>
 );
