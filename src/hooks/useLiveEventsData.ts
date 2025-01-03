02efc8d4f6eaed8f4de76bb68457b649cf485191
Angel <angel.pashov@proton.me>
2025-01-03 16:15:38 +0200

    Added refetch interval logic in useLiveEventsData hook

--- src/hooks/useLiveEventsData.ts
+++ src/hooks/useLiveEventsData.ts
@@ -1,34 +1,36 @@
-import {fetchLiveEventsData} from "../api/liveEventsApi.ts";
-import {Dispatch, useEffect, useState} from "react";
-import {useDispatch} from 'react-redux';
+import { fetchLiveEventsData } from "../api/liveEventsApi.ts";
+import { Dispatch, useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
 import {
     fetchLiveEventsSuccess,
     fetchLiveEventsFailure,
     LiveMatchesResponse
 } from "../store/slices/liveEventsSlice";
-import {useQuery} from "@tanstack/react-query";
-import {UnknownAction} from "@reduxjs/toolkit";
-
+import { useQuery } from "@tanstack/react-query";
+import { UnknownAction } from "@reduxjs/toolkit";
 
 export const useLiveEventsData = () => {
     const dispatch: Dispatch<UnknownAction> = useDispatch();
     const [enabled, setEnabled] = useState<boolean>(true);
 
-    const {data, error, isError, isSuccess} = useQuery<LiveMatchesResponse, Error>({
+    const { data, error, isError, isSuccess } = useQuery<LiveMatchesResponse, Error>({
         queryKey: ['liveEvents'],
         queryFn: fetchLiveEventsData,
+        refetchInterval: (data) => {
+            const liveData = data as unknown as LiveMatchesResponse; 
+            return liveData.matches?.length ? 10000 : false;
+        },
         enabled,
     });
 
     useEffect((): void => {
-        if(isSuccess && data) {
+        if (isSuccess && data) {
             dispatch(fetchLiveEventsSuccess(data));
             setEnabled(false);
+        } else if (isError && error) {
+            dispatch(fetchLiveEventsFailure(error.message));
         }
-        else if (isError && error) {
-            dispatch(fetchLiveEventsFailure((error as Error).message));
-        }
-    }, [data, error, isError, isSuccess])
+    }, [isSuccess, data, isError, error, dispatch]);
 
-    return {data, error, isError, isSuccess};
+    return { data, error, isError, isSuccess };
 };
