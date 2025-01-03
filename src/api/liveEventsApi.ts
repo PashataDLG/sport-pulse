7ede1be94bed2bb37e0611f14f67c3c8e256f2c1
Angel <angel.pashov@proton.me>
2025-01-03 16:18:15 +0200

    Added error handling

--- src/api/liveEventsApi.ts
+++ src/api/liveEventsApi.ts
@@ -1,14 +1,19 @@
-import axios, {AxiosResponse} from 'axios';
-import {LiveMatchesResponse} from "../store/slices/liveEventsSlice.ts";
+import axios, { AxiosResponse } from 'axios';
+import { LiveMatchesResponse } from "../store/slices/liveEventsSlice.ts";
 
 export const fetchLiveEventsData: () => Promise<LiveMatchesResponse> = async (): Promise<LiveMatchesResponse> => {
-    const response: AxiosResponse<LiveMatchesResponse> = await axios.get(
-        'https://api.football-data.org/v4/matches?status=LIVE',
-        {
-            headers: {
-                'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
-            },
-        }
-    );
-    return response.data;
+    try {
+        const response: AxiosResponse<LiveMatchesResponse> = await axios.get(
+            'https://api.football-data.org/v4/matches?status=LIVE',
+            {
+                headers: {
+                    'X-Auth-Token': '2b592fd358af4ea9b527e71282c29c54',
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching live events data:', error);
+        throw new Error('Failed to fetch live events data');
+    }
 };
