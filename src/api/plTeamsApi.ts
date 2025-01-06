7fe92cd8433024869d70b39ae0718c4663be034b
Angel <angel.pashov@proton.me>
2025-01-06 16:12:38 +0200

    Added error handling to PL teams api

--- src/api/plTeamsApi.ts
+++ src/api/plTeamsApi.ts
@@ -2,7 +2,10 @@ import axios, { AxiosResponse } from 'axios';
 import { TeamsResponse } from '../store/slices/plTeamsSlice';
 
 export const fetchPlTeams: () => Promise<TeamsResponse> = async (): Promise<TeamsResponse> => {
-    const response: AxiosResponse<TeamsResponse> = await axios.get('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League');
-
-    return response.data;
+    try {
+        const response: AxiosResponse<TeamsResponse> = await axios.get('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League');
+        return response.data;
+    } catch (error) {
+        throw new Error(`Failed to fetch Premier League teams due to: ${error}`);
+    }
 };
